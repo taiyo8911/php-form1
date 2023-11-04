931ccc34e3e3d3ec458574a95a0c195052cbd498
taiyo8911 <68884687+taiyo8911@users.noreply.github.com>
2023-11-04 09:07:52 +0900

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>入力データの加工結果</title>
+    </head>
+
+    <body>
+        <h1>入力データの加工結果</h1>
+        <?php
+        $name = $_POST["name"];
+        echo $name . "さん、こんにちは！";
+        ?>
+    </body>
+
+</html>
